60cd9d930449ad6165751ba66e26164d2f8c1f9f
avroraSolar <avrora.solar@ukr.net>
2023-10-03 17:01:13 +0300

    add traffic light

--- Jey_portfolio/main.js
+++ Jey_portfolio/main.js
@@ -54,6 +54,8 @@ car.showInfo();
 // Зміни часу на передану кількість хвилин.
 // Зміни часу на передану кількість годин.
 // Враховуйте, що в останніх 3-х функціях, при зміні однієї частини часу, може змінитися і інша. Наприклад: якщо до часу «20:59:45» додати 30 секунд, то повинно вийти «21:00:15», а не «20:59:75». Також потрібно передбачити можливість того що користувач може передати 150 секунд, або 75 хвилин.
+
+
 const time = {
     hours: 1,
     minutes: 2,
@@ -76,6 +78,7 @@ const time = {
             this.seconds = this.seconds%60;
         }
         if (this.minutes > 60) {
+            // this.getChangesMinutes(n);
             this.hours = this.hours + Math.trunc(this.minutes/60);
             this.minutes = this.minutes%60;
         }
@@ -109,6 +112,7 @@ time.showTime();
 const fraction = {
     divisible1: 1,
     divisor1: 2,
+
     resultFractional1() {
         result1 = `${this.divisible1}/${this.divisor1}`;
         console.log(result1);
@@ -175,3 +179,296 @@ console.log(fraction.divide());
 //   a += b;
 //   alert(`The greatest common divisor of ${number1} and ${number2} is ${a}`);
 // }
+
+const arr = [1,2,3,4,5,6];
+const arr1 = ['cat', 'banana', 'Apple', 'tomato', 'Cucumber', 'apple', 'orange', 'dog'];
+const lowerCaseArr = arr1.map(el => el.toLowerCase());
+lowerCaseArr.sort();
+console.log(lowerCaseArr);
+
+const num = [1,5,2,15,10000,16.6,-5,-1];
+const sortFunction = function(a, b) {
+    if(a > b) {
+        return 1;
+    } else if (a < b) {
+        return-1;
+    } else {
+        return 0;
+    }
+}
+const sortFunction1 = function(a, b) {
+    return a - b;
+}
+// num.sort((a, b) => a - b); сортування за зростанням
+
+// num.sort((a, b) => b - a); сортування за зменшенням
+num.sort();
+const reversWord = 'tomato'.split('').reverse().join('');
+console.log(reversWord);
+const toReverse = word => word.split('').reverse().join('');
+const reversedArr = arr1.map.toReverse;
+console.log(reversedArr);
+
+
+const allProducts = [
+    {
+        name: 'banana',
+        unicId: 12358,
+        price: 8,
+        quantity:200,
+        discount: 0.03,
+        isDiscount: true,
+    },
+    {
+        name: 'cucumber',
+        unicId: 1238,
+        price: 18,
+        quantity: 100,
+        discount: 0.03,
+        isDiscount: true,
+    },
+    {
+        name: 'apple',
+        unicId: 12300258,
+        price: 10,
+        quantity: 50,
+        discount: 0.3,
+        isDiscount: true,
+    },
+    {
+        name: 'potato',
+        unicId: 12336658,
+        price: 84,
+        quantity: 500,
+        discount: 0,
+        isDiscount: true,
+    },
+    {
+        name: 'banana',
+        unicId: 12358,
+        price: 8,
+        quantity:200,
+        discount: 0.03,
+        isDiscount: true,
+    }
+]
+
+
+const result = arr.indexOf(2);
+console.log(result);
+
+// Створи масив «Список покупок». Кожен елемент масиву є об'єктом, який містить назву продукту, кількість і куплений він чи ні, ціну за одиницю товару, сума. Написати кілька функцій для роботи з таким масивом:
+// Виводити весь список на екран таким чином, щоб спочатку йшли продукти, що ще не придбані, а потім - ті, що вже придбали.
+// Покупка продукту. Функція приймає назву продукту і відзначає його як придбаний.
+
+const shoppingList = [
+    {
+      productName: "banana",
+      quantity: 100,
+      added: true,
+      price: 44,
+      amount: 4400
+    },
+    {
+      productName: "cucumber",
+      quantity: 50,
+      added: false,
+      price: 39,
+      amount: 1950
+    },
+    {
+      productName: "tomato",
+      quantity: 20,
+      added: true,
+      price: 50,
+      amount: 1000
+    },
+    {
+      productName: "pepper",
+      quantity: 25,
+      added: false,
+      price: 30,
+      amount: 750
+    },
+    {
+      productName: "peach",
+      quantity: 10,
+      added: true,
+      price: 85,
+      amount: 850
+    },
+    {
+      productName: "banana",
+      quantity: 100,
+      added: true,
+      price: 44,
+      amount: 4400
+    },
+    {
+      productName: "candy",
+      quantity: 5,
+      added: false,
+      price: 200,
+      amount: 1000
+    }
+  ]
+  console.log(shoppingList[3].added);
+  // shoppingList[added]=false;
+  // console.log(shoppingList);
+  // console.log()
+  // console.log(productName('candy'));
+  
+  function sortShoppingList(name) {
+    return (a, b) => a[name] > b[name] ? 1 : -1;
+  }
+  
+  function purchaseProduct(name) {
+    const buyProduct = shoppingList.map((el) => (
+        el.productName === name
+          ? { ...el, added: true }
+          : el
+      ));
+      console.log(buyProduct);
+      return buyProduct;
+  }
+  
+  
+    console.log(purchaseProduct("candy"));
+    console.log(purchaseProduct("pepper"));
+    console.log(shoppingList);
+    purchaseProduct("candy");
+    console.log(shoppingList);
+    // console.log(shoppingList(purchaseProduct(candy)));
+    // shoppingList[buyProduct].added = true;
+  
+    // purchaseProduct('candy');
+  
+    function setQuantity(quantity, id) {
+      const newProd = allProducts.findIndex((el) => el.uniqId === id);
+      allProducts[newProd].quantity = quantity;
+    }
+    // shoppingList.map((el) => el.productName) {
+    //   shoppingList.added = true;
+    //   console.log(shoppingList);
+    // }
+    // console.log(shoppingList.sort(sortShoppingList('added')));
+    // console.log(shoppingList(purchaseProduct('candy')));
+    // console.log(buyProduct);
+  
+  
+//     const allProducts = [
+//     {
+//         name: "banana",
+//         uniqId: 74638,
+//         price: 8,
+//         quantity: 200,
+//         discount: 0.03,
+//         isDiscount: true,
+//       },
+//       {
+//         name: "cucumber",
+//         uniqId: 2334,
+//         price: 4,
+//         quantity: 100,
+//         discount: 0,
+//         isDiscount: false,
+//       },
+//       {
+//         name: "pear",
+//         uniqId: 234,
+//         price: 18,
+//         quantity: 0,
+//         discount: 0.5,
+//         isDiscount: true,
+//       },
+//       {
+//         name: "apple",
+//         uniqId: 3322,
+//         price: 18,
+//         quantity: 2,
+//         discount: 0.1,
+//         isDiscount: true,
+//       },
+//       {
+//         name: "potato",
+//         uniqId: 333,
+//         price: 2,
+//         quantity: 2000,
+//         discount: 0,
+//         isDiscount: false,
+//       },
+//       {
+//         name: "coconut",
+//         uniqId: 3498,
+//         price: 80,
+//         quantity: 10,
+//         discount: 0.1,
+//         isDiscount: true,
+//       },
+//     ];
+//     const userCart = [];
+  
+//     function addToCard(id, quantity) {
+//       const newProd = allProducts.find((el) => el.uniqId === id);
+//       if (!newProd) {
+//         console.log(`det finns inte`);
+//         return false;
+//       }
+//       if (newProd.quantity < quantity) {
+//         console.log(`Du kan inte köpa det`);
+//         return false;
+//       }
+//       userCart.push(newProd);
+//       setQuantity(newProd.quantity - quantity, id);
+//       // console.log(newProd);
+//     }
+//     addToCard(333, 10);
+  
+//     function setQuantity(quantity, id) {
+//       const newProd = allProducts.findIndex((el) => el.uniqId === id);
+//       allProducts[newProd].quantity = quantity;
+//     }
+  
+//     addToCard(3498, 2);
+//     addToCard(234, 3);
+  
+//     function deleteProduct(id) {
+//       const deletedProduct = userCart.findIndex(el => el.id === id);
+//       userCart.splice(deletedProduct, 1);
+//     }
+//     // deleteProduct(234);
+//     function showCardProduct() {
+//       const result = userCart.map(el => {
+//         return {
+//           name: el.name,
+//           price: el.price
+//         };
+//       });
+//       console.log(result);
+//     }
+//     showCardProduct();
+
+// posts = [
+//     {
+//         "id": 1,
+//             "text": "One",
+//             "likes": "1" 
+//     },
+//     {
+//         "id": 2,
+//             "text": "Two",
+//             "likes": 50 
+//     },
+//     {
+//         "id": 3,
+//             "text": "Three",
+//             "likes": 7 
+//     }
+//     ]
+//     const newPosts = posts.map((post) => (
+//         post.id === 1
+//           ? { ...post, text: 'other text' }
+//           : post
+//       ));
+//       console.log(posts);
+//       console.log(newPosts);
---
+++ traffic-light/main.js
@@ -0,0 +1,88 @@
+// function clearLights() {
+//     document.querySelectorAll('.bulb').forEach( (bulb) => {
+//         bulb.style.background = 'black';
+//     });
+//   }
+  
+//   function illuminateStopLight() {
+//     clearLights();
+//     document.querySelector('#stopLight').style.backgroundColor = 'red';
+//   }
+  
+//   function illuminateSlowLight() {
+//     clearLights();
+//     document.querySelector('#slowLight').style.backgroundColor = 'orange';
+//   }
+  
+//   function illuminateGoLight() {
+//     clearLights();
+//     document.querySelector('#goLight').style.backgroundColor = 'green';
+//   }
+  
+//   window.onload = function(){
+  
+//     document.querySelector('#slowButton')
+//             .addEventListener('click', illuminateSlowLight);
+  
+//     document.querySelector('#stopButton')
+//             .addEventListener('click', illuminateStopLight);
+  
+//   };
+//   document.querySelector('#goButton')
+//   .addEventListener('click', illuminateGoLight);
+
+;(function(doc){
+    let runLight = false;
+    let interval;
+    let step = 1; let count = 0;
+
+    function removeClasses(elem, colors) {
+        let len = colors.length;
+        for(item of elem) {
+            for(let i = 0; i < len; i++) {
+                if(item.classList.contains(colors[i])) {
+                    item.classList.remove(colors[i]);
+                }
+            }
+        }
+    }
+
+    function runInterval(elem, colors) {
+        elem[count].classList.add(colors[count]);
+        let len = elem.length;
+        interval = setInterval(function(){
+            removeClasses(elem, colors);
+            count += step;
+            elem[count].classList.add(colors[count]);
+            if(count === len - 1 || count === 0) {
+                step = -step;
+            }
+        }, 1000);
+    }
+
+    function run() {
+        let light = doc.querySelectorAll('.circle');
+        if(light !== null) {
+            let lightColor = ["red", "yellow", "green"];
+            runInterval(light, lightColor);
+        }
+    }
+
+    function start() {
+        let btn = doc.querySelector('.btn-run');
+        if(btn !== null) {
+            btn.onclick = function(event) {
+                runLight = !runLight;
+                if(runLight) {
+                    run();
+                    btn.innerText = "Pause";
+                }else {
+                    clearInterval(interval);
+                    btn.innerText = "Run";
+                }
+            }
+        }
+    }
+
+    start();
+})(document);
